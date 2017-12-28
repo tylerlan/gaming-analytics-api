c75edf521b829ed498f4ead3f4920afc4d192be5
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-12-28 00:35:01 -0800

    Add method to normalize metric keys between pupd and aggr

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,11 +7,23 @@ const organizeDataByMetric = arrayOfDays => {
     organizedData.dates.push(day['date']);
 
     // store the values for each metric by date
-    // omit, date, dateTicks, and machineDays -- they aren't necessary for frontend
     for (let prop in day) {
+      // omit, date, dateTicks, and machineDays -- they aren't necessary for frontend
       if (prop !== 'date' && prop !== 'dateTicks' && prop !== 'machineDays') {
-        if (!organizedData.metrics[prop]) organizedData.metrics[prop] = {};
-        organizedData.metrics[prop][day['date']] = day[prop];
+        // save the original name (in case it changes)
+        let originalName = prop;
+
+        // remove PUPD if it's in the last 4 characters
+        // THIS STEP MAKES METRICS MORE CONSISTENTLY LABELED ACROSS DATASETS
+        if (prop.substr(prop.length - 4) === 'PUPD') {
+          prop = prop.slice(0, prop.length - 4);
+        }
+
+        if (!organizedData.metrics[prop]) {
+          organizedData.metrics[prop] = {};
+        }
+
+        organizedData.metrics[prop][day['date']] = day[originalName];
       }
     }
   });
