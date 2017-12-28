fe02a3d27329c6ab2074674670b0cc2e53e6b350
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-12-27 23:17:56 -0800

    Add boom to async middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,13 @@
+const boom = require('boom');
 // MIDDLEWARE WRAPPER MAKES SURE ERRORS GET PASSED TO NEXT
+
 const asyncMiddleware = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+  Promise.resolve(fn(req, res, next)).catch(err => {
+    if (!err.isBoom) {
+      return next(boom.badImplementation(err));
+    }
+    next(err);
+  });
 };
 
 module.exports = {
